2241747f8aab10e1d5655650815a21d6259fbd8c
WitchElaina <msz13399173387@gmail.com>
2022-05-04 21:07:37 +0800

    Update adht version

--- adafruitDHT.py
+++ adafruitDHT.py
@@ -2,15 +2,21 @@
 Adafruit Mod
 """
 import time
+import board
 import PCF8591 as ADC
-import Adafruit_DHT
+import adafruit_dht
 
 sensor_arg = '11'
-
-def DHT_read(sensor=sensor_arg, pin=0):
-    return Adafruit_DHT.read_retry(sensor, pin)
+dhtDevice = adafruit_dht.DHT22(board.D18)
 
 def DHT_mainloop(GPIO_pin):
     while True:
-        print(DHT_read(sensor_arg, GPIO_pin))
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
         time.sleep(1)
