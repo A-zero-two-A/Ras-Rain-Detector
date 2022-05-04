f846329b03ad33263e18cfdc4948e269cf08a5cc
WitchElaina <msz13399173387@gmail.com>
2022-05-04 19:39:45 +0800

    fix bugs

--- touch.py
+++ touch.py
@@ -1,18 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-makerobo_TouchPin = 16   # 触摸传感器管脚PIN
+makerobo_TouchPin = 36   # 触摸传感器管脚PIN
 
 makerobo_tmp = 0    #是否有触摸判断 
 
-# GPIO口定义
-def makerobo_setup():
-	GPIO.setmode(GPIO.BOARD)       # 采用实际的物理管脚给GPIO口
-	GPIO.setwarnings(False)        # 忽略GPIO操作注意警告
-	GPIO.setup(makerobo_TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # 设置makerobo_TouchPin管脚为输入模式，上拉至高电平(3.3V)
+
+GPIO.setmode(GPIO.BOARD)       # 采用实际的物理管脚给GPIO口
+GPIO.setwarnings(False)        # 忽略GPIO操作注意警告
+GPIO.setup(makerobo_TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # 设置makerobo_TouchPin管脚为输入模式，上拉至高电平(3.3V)
 
 # 打印函数，显示出是否有触摸
-def is_touch(x=GPIO.input(makerobo_TouchPin)):
+def is_touch():
 	global makerobo_tmp
 	if x != makerobo_tmp:
 		if x == 1:
@@ -23,7 +22,7 @@ def is_touch(x=GPIO.input(makerobo_TouchPin)):
   
 if __name__ == '__main__':
     while True:
-        cur = is_touch()
+        cur = is_touch(GPIO.input(makerobo_TouchPin))
         if cur:
             print('1')
         else:
