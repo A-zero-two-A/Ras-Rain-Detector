df105558044609d6c1da120db7479a434739ed60
WitchElaina <msz13399173387@gmail.com>
2022-05-04 19:43:00 +0800

    Fix touch

--- touch.py
+++ touch.py
@@ -11,7 +11,7 @@ GPIO.setwarnings(False)        # 忽略GPIO操作注意警告
 GPIO.setup(makerobo_TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # 设置makerobo_TouchPin管脚为输入模式，上拉至高电平(3.3V)
 
 # 打印函数，显示出是否有触摸
-def is_touch():
+def is_touch(x):
 	global makerobo_tmp
 	if x != makerobo_tmp:
 		if x == 1:
