6b5248d665fc292d092999c05764ac338abeff6d
WitchElaina <msz13399173387@gmail.com>
2022-05-04 21:10:14 +0800

    Fix Runtime err

--- adafruitDHT.py
+++ adafruitDHT.py
@@ -11,12 +11,22 @@ dhtDevice = adafruit_dht.DHT22(board.D18)
 
 def DHT_mainloop(GPIO_pin):
     while True:
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
+        try:
+            temperature_c = dhtDevice.temperature
+            temperature_f = temperature_c * (9 / 5) + 32
+            humidity = dhtDevice.humidity
+            print(
+                "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                    temperature_f, temperature_c, humidity
+                )
             )
-        )
-        time.sleep(1)
+            time.sleep(1)
+        except RuntimeError as error:
+            # Errors happen fairly often, DHT's are hard to read, just keep going
+            print(error.args[0])
+            time.sleep(2.0)
+            continue
+        except Exception as error:
+            dhtDevice.exit()
+            raise error    
+    
