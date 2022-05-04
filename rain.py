9544090422713c070c07713782e2b9aa5d4dc359
WitchElaina <msz13399173387@gmail.com>
2022-05-04 20:20:02 +0800

    Fix rain

--- rain.py
+++ rain.py
@@ -3,12 +3,9 @@ import PCF8591 as ADC
 def rain_mainloop(ADC_addr=0):
     rain_status = 1
     while True:
-        print (ADC.read(ADC_addr))
-        # rain_tmp = GPIO.input(pin)
-        # if rain_tmp != rain_status:
-        #     if rain_tmp:
-        #         print("Raining")   
-        #     else:
-        #         print("Not rain")
-        #     rain_status = rain_tmp 
+        cur = ADC.read(ADC_addr)
+        if cur <= 100:
+            print("Raining..")
+        else:
+            print("Not rain")
         time.sleep(1)  
