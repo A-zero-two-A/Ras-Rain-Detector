e10d69a88ddea818e9e2fc94c725a9d41d212db4
WitchElaina <msz13399173387@gmail.com>
2022-05-04 20:32:40 +0800

    Fix ADC port bug

--- test.py
+++ test.py
@@ -3,12 +3,18 @@ import thermistor
 import PCF8591 as ADC
 import RPi.GPIO as GPIO
 
-PCF_addr = '0x48'
+ADC_addr = {
+    'rain': 0,
+    'aDHT': 1,
+    'thm': 2
+}
+
+PCF_addr = 0x48
 
 def init():
     ADC.setup(PCF_addr)
 
 if __name__ == '__main__':
     init()
-    # rain.rain_mainloop(PCF_addr)
-    thermistor.thm_loop(PCF_addr)
+    # rain.rain_mainloop(ADC_addr['rain'])
+    thermistor.thm_loop(ADC_addr['thm'])
