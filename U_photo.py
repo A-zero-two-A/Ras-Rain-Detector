fc843edb4bb27ff05339c0393dc37375ccb27a93
WitchElaina <msz13399173387@gmail.com>
2022-05-05 11:52:08 +0800

    Fix event listener

--- U_photo.py
+++ U_photo.py
@@ -19,6 +19,8 @@ def U_photo_Print(x):
 # 中断函数，检测到有物体挡住时，响应中断函数					
 def U_photo_detect():
 	U_photo_Print(GPIO.input(U_photo_PIPin))   # 打印出检测到有物体挡住！
+ 
+GPIO.add_event_detect(U_photo_PIPin, GPIO.BOTH, callback=U_photo_detect, bouncetime=200)
 
 # 循环函数
 def loop():
@@ -30,9 +32,7 @@ def destroy():
 
 # 程序入口
 if __name__ == '__main__':
-    
     try:
-        GPIO.add_event_detect(U_photo_PIPin, GPIO.BOTH, callback=U_photo_detect, bouncetime=200)
         loop()
     except KeyboardInterrupt:
         destroy()
