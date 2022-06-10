ffc6200592042dc37738d76b8e6568ae1aeff5ae
WitchElaina <msz13399173387@gmail.com>
2022-06-10 22:29:27 +0800

    Fix arg error

--- Ras-Rain-Detector.py
+++ Ras-Rain-Detector.py
@@ -67,7 +67,7 @@ class RainDet(object):
         else:
             self.warn = False
 
-    def up_callback(self):
+    def up_callback(self, args=None):
         """
         判断窗户是否打开
         :return: 设置成员win_closed的值
